ef2c655c9db9b0620492dc4c14cd0a38dab9fde1
Eduardo Nunes <nunes_eduardo@rocketmail.com>
2019-05-14 23:02:25 -0300

    Melhorando a lógica de funcionamento do bot

--- App.py
+++ App.py
@@ -7,8 +7,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 TOKEN = "892737322:AAGYB5X5cJnFyLbjsBSQ8lw4t1tYAPvR2t4"
-BASE_URL = "http://fipeapi.appspot.com/api/1/"
-URL = ""
+BASE_URL = "http://fipeapi.appspot.com/api/1"
 
 bot = telegram.Bot(token=TOKEN)
 updater = Updater(token=TOKEN)
@@ -73,71 +72,76 @@ def t2(bot, update, args):
     print("ARGS: ", args)
 
     ############################################
-    print("TEM MARCA? ", marca)
-    if len(args) == 0 and not marca:
+    if len(args) == 0:
         bot.sendMessage(chat_id=chat_id, text="Insira a marca de seu veículo")
     else:
-        marca = args[0]
+        if not marca:
+            bot.sendMessage(chat_id=chat_id, text="Recuperando dados de marcas de veículos, aguarde...")
+            URL = "{}/carros/marcas.json".format(BASE_URL)
 
-        bot.sendMessage(chat_id=chat_id, text="Recuperando dados de marcas de veículos, aguarde...")
-        response = requests.get(BASE_URL + "/marcas.json")
-        marcas = json.loads(response.content)
-        bot.sendMessage(chat_id=chat_id, text="Dados carregados! :D")
+            response = requests.get(URL)
 
-        bot.sendMessage(chat_id=chat_id, text="Recuperando modelos de {}".format(marca))
+            marcas = json.loads(response.content)
 
-        for m in marcas:
-            if marca.upper() == m['name']:
-                URL = BASE_URL + "/veiculos/" + str(m['id']) + ".json"
-                break
+            len_marcas = len(marcas)
+
+            print("{} marcas".format(len_marcas))
+
+            bot.sendMessage(chat_id=chat_id, text="Dados carregados! :D")
+
+            marca = args[0]
+
+            bot.sendMessage(chat_id=chat_id, text="Recuperando modelos de {}".format(marca))
+
+            URL = ""
+            for m in marcas:
+                if marca.upper() == m['name']:
+                    URL = "{}/carros/veiculos/{}.json".format(BASE_URL, m['id'])
+                    break
         
-        response = requests.get(URL)
+            response = requests.get(URL)
 
-        bot.sendMessage(chat_id=chat_id, text="Modelos recuperados! :D")
+            bot.sendMessage(chat_id=chat_id, text="Modelos recuperados! :D")
 
-        modelos = json.loads(response.content)
-        print(modelos[0])
+            modelos = json.loads(response.content)
+            print(modelos[0])
 
-        total_veiculos = len(modelos)
+            total_veiculos = len(modelos)
 
-        bot.sendMessage(chat_id=chat_id, text="Encontrei {} {}. Aqui estão alguns deles: ".format(total_veiculos,
-                        "modelos" if total_veiculos > 1 else "modelo"))
+            bot.sendMessage(chat_id=chat_id, text="Encontrei {} {}. Aqui estão alguns deles: ".format(total_veiculos,
+                            "modelos" if total_veiculos > 1 else "modelo"))
         
-        q = 0
-        if update.callback_query:
-            q = int(update.callback_query.data)
-            print("CALLBACK", q)
-        print("Q ->",q)
-        print("C ->", c)
-        for m in modelos[c:c+q+1]:
-            bot.sendMessage(chat_id=chat_id, text="{} - {}".format(m['id'], m['name']))
-        c  += q
-        keyboard = [[
-            InlineKeyboardButton('Listar +20 modelos', callback_data='20')
-        ]]
-        bot.sendMessage(chat_id=chat_id, text=None, reply_markup=InlineKeyboardMarkup(keyboard))
+            for m in modelos[:11]:
+                bot.sendMessage(chat_id=chat_id, text="ID: {} - {}".format(m['id'], m['name']))
+
+            bot.sendMessage(chat_id=chat_id, text="Escolha o id de seu veículo")
+        else:
+            modelo_id = args[0]
+
+            print(modelos[:3])
+
+            URL = ""
+            found = False
+
+            for m in modelos:
+                if modelo_id == m['id']:
+                    URL = "{}/carros/veiculo/{}/{}.json".format(BASE_URL, m['id'], modelo_id)
+                    found = True
+                    break
+            
+            response = requests.get(URL)
+
+            modelo = json.loads(response.content)
+
+            if not found:
+                bot.sendMessage(chat_id=chat_id, text="Não achei este veículo :(")
+            else:
+                bot.sendMessage(chat_id=chat_id, text="{}".format(json.dumps(modelo)))
+                bot.sendMessage(chat_id=chat_id, text="CABO MAN")
                 
 
     ############################################
-    '''
-    if not modelo:
-        bot.sendMessage(chat_id=chat_id, text="Escolha o id de seu veículo")
-    else:
-        modelo_id = args[0]
-
-        if modelo_id in modelos_dict.keys():
-            modelo = modelos_dict[modelo_id]['nome']
-
-            for m in modelos_dict:
-                if modelo.upper() == m['name'].upper():
-                    URL = "{}/veiculo/{}/{}.json".format(BASE_URL, m['id'], modelo['id'])
-                    break
-        else:
-            bot.sendMessage(chat_id=chat_id, text="Não achei esse número: {}".format(modelo_id))
     
-
-    ############################################
-    '''
     #if not ano_combustivel:
     #    bot.sendMessage(chat_id=chat_id, text="Insira o ano/combustível de seu veículo")
     #else:
@@ -146,7 +150,6 @@ def t2(bot, update, args):
 
 def help(self, updater):
     pass
-
 
 dispatcher.add_handler(CommandHandler('t', test, pass_args=True))
 dispatcher.add_handler(CommandHandler('t2', t2, pass_args=True))
